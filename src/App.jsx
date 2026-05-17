9ec169eb5cd27d94b31d171d35793435fc2d2f80
MrNaisuGuy <mrnaisuguy@gmail.com>
2026-05-17 08:41:40 +0000

    Adding files.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main>
+      <h1>DojoTCG</h1>
+      <p>Card scanner prototype</p>
+      <button>Upload Card</button>
+    </main>
+  );
+}
+
+export default App;
